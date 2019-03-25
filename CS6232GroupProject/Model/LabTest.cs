a51f190b99c7c2376ae286ac865cce286d1ce016
Michael Windsor <mwindso1@my.westga.edu>
2019-03-25 19:28:21 -0400

    Added DB classes to the Model.

---
+++ CS6232GroupProject/Model/LabTest.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace CS6232GroupProject.Model
+{
+    /// <summary>
+    /// This class creates a LabTest object with auto-implemented properties.
+    /// </summary>
+    class LabTest
+    {
+        public int TestID {get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CS6232GroupProject/Model/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS6232GroupProject.Model
+{
+    /// <summary>
+    /// This class creates a Patient object with auto-implemented properties.
+    /// </summary>
+    class Patient
+    {
+        public int PatientID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string SSN { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public int AddressID { get; set; }
+    }
+}
