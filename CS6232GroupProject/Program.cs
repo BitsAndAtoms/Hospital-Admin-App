efd2b8510ec210a01d579ecc62d38dcfab6f3f8c
Michael Windsor <mwindso1@my.westga.edu>
2019-03-25 18:51:37 -0400

    Removed all unused imports.

--- CS6232GroupProject/Program.cs
+++ CS6232GroupProject/Program.cs
@@ -1,8 +1,5 @@
 ﻿using CS6232GroupProject.View;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CS6232GroupProject
