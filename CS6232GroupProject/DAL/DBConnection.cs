5c25a9ea899b0559ae5dee81c818ddeaf417c683
Sid Gupta <sgupta2@my.westga.edu>
2019-03-27 02:02:43 -0500

    I have configured the DB connection

---
+++ CS6232GroupProject/DAL/DBConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6232GroupProject.DAL
+{
+    /// <summary>
+    /// method for creating a connection between C# and DB
+    /// </summary>
+    class DBConnection
+    {
+        /// <summary>
+        /// new sql connection is generated
+        /// </summary>
+        /// <returns>connection is connection to DB</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=Clinic;" +
+                "Integrated Security=True";
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
