571d6bfed37f70b8aee3f85f98d7dc5ac3564877
Michael Windsor <mwindso1@my.westga.edu>
2019-03-27 12:44:10 -0400

    Merging to master.

--- CS6232GroupProject/Controller/Program.cs
+++ CS6232GroupProject/Controller/Program.cs
@@ -7,7 +7,7 @@ namespace CS6232GroupProject
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. 
         /// </summary>
         [STAThread]
         static void Main()
