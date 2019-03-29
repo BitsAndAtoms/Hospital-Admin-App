f00e527a5f8aa1c3df509e489debecb3b8c907c4
Robert Chesser <rchesser@westga.edu>
2019-03-29 11:22:11 -0400

    Visual updates.

--- CS6232GroupProject/View/FormNurseMain.Designer.cs
+++ CS6232GroupProject/View/FormNurseMain.Designer.cs
@@ -39,6 +39,7 @@
             // labelName
             // 
             this.labelName.AutoSize = true;
+            this.labelName.BackColor = System.Drawing.Color.Transparent;
             this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelName.Location = new System.Drawing.Point(136, 60);
             this.labelName.Name = "labelName";
@@ -50,18 +51,20 @@
             // 
             this.linkLabelLogout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabelLogout.AutoSize = true;
-            this.linkLabelLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabelLogout.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(189)))));
+            this.linkLabelLogout.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabelLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelLogout.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(63)))), ((int)(((byte)(100)))));
             this.linkLabelLogout.Location = new System.Drawing.Point(656, 0);
             this.linkLabelLogout.Name = "linkLabelLogout";
             this.linkLabelLogout.Padding = new System.Windows.Forms.Padding(0, 10, 10, 0);
-            this.linkLabelLogout.Size = new System.Drawing.Size(82, 35);
+            this.linkLabelLogout.Size = new System.Drawing.Size(88, 35);
             this.linkLabelLogout.TabIndex = 2;
             this.linkLabelLogout.TabStop = true;
             this.linkLabelLogout.Text = "Logout";
             // 
             // pictureBoxNurseIcon
             // 
+            this.pictureBoxNurseIcon.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxNurseIcon.Image = global::CS6232GroupProject.Properties.Resources.nurseicon;
             this.pictureBoxNurseIcon.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxNurseIcon.Name = "pictureBoxNurseIcon";
@@ -84,6 +87,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::CS6232GroupProject.Properties.Resources.bggradient_topsmall;
             this.ClientSize = new System.Drawing.Size(738, 750);
             this.Controls.Add(this.userControlNurseMain1);
             this.Controls.Add(this.linkLabelLogout);
