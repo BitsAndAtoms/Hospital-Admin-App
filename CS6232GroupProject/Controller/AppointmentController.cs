81bbe25fcb8d633ccfc8dcb88c4de6b4b68a33f9
Michael Windsor <mwindso1@my.westga.edu>
2019-03-31 16:30:28 -0400

    Created the AppointmentController to call the AppointmentDAL methods.

--- CS6232GroupProject/Controller/AppointmentController.cs
+++ CS6232GroupProject/Controller/AppointmentController.cs
@@ -1,14 +1,35 @@
 ﻿using CS6232GroupProject.DAL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CS6232GroupProject.Model;
 
 namespace CS6232GroupProject.Controller
 {
+    /// <summary>
+    /// This class creates an AppointmentController object 
+    /// and calls the AppointmentDAL methods.
+    /// </summary>
     class AppointmentController
     {
         private readonly AppointmentDAL appointmentSource;
+
+        /// <summary>
+        /// This constructs the Appointment Controller object and 
+        /// initialises the AppointmentDAL variable.
+        /// </summary>
+        public AppointmentController()
+        {
+            this.appointmentSource = new AppointmentDAL();
+        }
+
+        /// <summary>
+        /// This method passes an Appointment object to the AppointmentDAL 
+        /// which updates the DB with that information and returns the ID 
+        /// of the appointment.
+        /// </summary>
+        /// <param name="appointment"></param>
+        /// <returns>The ID of the appointment created.</returns>
+        public int CreateAppointment(Appointment appointment)
+        {
+            return this.appointmentSource.CreateAppointment(appointment);
+        }
     }
 }
