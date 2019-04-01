61cfe38322de6488bc580e8bdd5d03a0e01a6ee7
Michael Windsor <mwindso1@my.westga.edu>
2019-04-01 01:34:55 -0400

    Added PatientVisitController.

---
+++ CS6232GroupProject/Controller/PatientVisitController.cs
@@ -0,0 +1,20 @@
+﻿using CS6232GroupProject.DAL;
+using CS6232GroupProject.Model;
+
+namespace CS6232GroupProject.Controller
+{
+    class PatientVisitController
+    {
+        private readonly PatientVisitDAL visitSource;
+
+        public PatientVisitController()
+        {
+            this.visitSource = new PatientVisitDAL();
+        }
+
+        public PatientVisit GetPatientVisitInfoByAppointment(int appointmentID)
+        {
+            return this.visitSource.GetPatientVisitInfoByAppointment(appointmentID);
+        }
+    }
+}
