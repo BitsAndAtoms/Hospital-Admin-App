de4277d565b546b972b071846e3850949956b0f2
Michael Windsor <mwindso1@my.westga.edu>
2019-04-07 19:59:23 -0400

    Created the NurseController with a method to call the GetNurses method from NurseDAL.

---
+++ CS6232GroupProject/Controller/NurseController.cs
@@ -0,0 +1,32 @@
+﻿using CS6232GroupProject.DAL;
+using CS6232GroupProject.Model;
+using System.Collections.Generic;
+
+namespace CS6232GroupProject.Controller
+{
+    /// <summary>
+    /// This class allows the calling of methods in the NurseDAL, to 
+    /// get Nurse data from the DB.
+    /// </summary>
+    class NurseController
+    {
+        private readonly NurseDAL nurseSource;
+
+        /// <summary>
+        /// This constructs the NurseController.
+        /// </summary>
+        public NurseController()
+        {
+            this.nurseSource = new NurseDAL();
+        }
+
+        /// <summary>
+        /// This method returns a list of all Nurse objects.
+        /// </summary>
+        /// <returns>A list of Nurse objects. </returns>
+        public List<Nurse> GetNurses()
+        {
+            return this.nurseSource.GetNurses();
+        }
+    }
+}
